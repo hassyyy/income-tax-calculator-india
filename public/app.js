c4f10655963c2700b4f32d7113869e185c8d9331
MohamedAsan <mohamed.asan@freshworks.com>
2021-11-13 23:03:06 +0530

    Style form

--- public/app.js
+++ public/app.js
@@ -1,1 +1,9 @@
 console.log("App JS");
+
+inputElements = ['#ctc', '#pf', "#hra", '#deductions-80c', '#deductions-others']
+currencyOptions = {
+  currencySymbol : '₹',
+  digitalGroupSpacing: '2',
+  allowDecimalPadding: false
+}
+AutoNumeric.multiple(inputElements, currencyOptions);
